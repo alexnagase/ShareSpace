fcb486ee7cd78e3df45f4c582a8bd5f142ecdfda
Alex Nagase <alex@umail.ucsb.edu>
2017-08-12 12:26:29 -0700

    Sharespace app

---
+++ app/src/main/java/com/example/alex/sharespace/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.alex.sharespace;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.sharebutton) {
+            Intent myIntent = new Intent(MainActivity.this, Share.class);
+
+            MainActivity.this.startActivity(myIntent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
