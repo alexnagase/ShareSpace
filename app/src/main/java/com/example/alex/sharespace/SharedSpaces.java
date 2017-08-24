061732cbad38c8adc5842d422bd3fced11085492
Alex Nagase <alex@umail.ucsb.edu>
2017-08-24 12:15:35 -0700

    added activity to view shared spaces

---
+++ app/src/main/java/com/example/alex/sharespace/SharedSpaces.java
@@ -0,0 +1,13 @@
+package com.example.alex.sharespace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SharedSpaces extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_spaces);
+    }
+}
